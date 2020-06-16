1133fdce6b4ff9e7a037a30092074328bf54d794
Tom Slater <TomSlater@users.noreply.github.com>
2020-06-16 10:50:30 +0100

    Update test_ase_import.py

--- atomap/tests/test_ase_import.py
+++ atomap/tests/test_ase_import.py
@@ -1,6 +1,6 @@
 from atomap.convert_ase import load_ase
 from ase.cluster import Octahedron
-from unittest import assertAlmostEqual
+import math
 
 
 class TestASEImport:
@@ -10,7 +10,7 @@ class TestASEImport:
         atomlattice = load_ase(atoms, (128,128), gaussian_blur=1)
 
         assert atomlattice.sublattice_list[0].atom_list[0].pixel_x == 0.0
-        assertAlmostEqual(atomlattice.sublattice_list[0].atom_list[0].pixel_y,
-                          29.09)
+        assert math.isclose(atomlattice.sublattice_list[0].atom_list[0].pixel_y,
+                          29.09, abs_tol=0.01)
         assert atomlattice.sublattice_list[0].atom_list[0].element_info == {
             2.045: 'Ag', 6.135: 'Ag'}
